6e123a048e1edc0abbf0b377f9da20e2322168d0
Diego Gómez <2833316742101@ingenieria.usac.edu.gt>
2026-08-02 16:56:26 -0600

    regresar

--- Tarea2/src/Main.java
+++ Tarea2/src/Main.java
@@ -12,14 +12,26 @@ public class Main{
     static void ingresoDeDatos(Scanner read, int[] notas, int i){
         System.out.println("Ingresa tu nombre");
         String name = read.nextLine();
+        boolean value=true;
         System.out.println("Ingresa tus notas (0-10): ");
         for (i=0; i<5; i++ ){
-            if (read.hasNextInt() && notas[i]<10){
+            if (read.hasNextInt()){
                 notas[i]=read.nextInt();
+                if (notas[i]<10 && notas[i]>=0){
+                    System.out.println("Valor guardado");
+                }
+                else{
+                    System.out.println("Nota no válida");
+                }
             }
             else{
                 System.out.println("El valor que has ingresado no es válido");
                 read.next();
+                while(value){
+                    System.out.println("Ingresa de nuevo el valor");
+                    notas[i]=read.nextInt();
+                }
+
             }
         }
     }

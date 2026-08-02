743ae296e351781aebbdad7fbb5307e9f39803fa
Diego Gómez <2833316742101@ingenieria.usac.edu.gt>
2026-08-02 16:36:14 -0600

    Validación de datos

--- Tarea2/src/Main.java
+++ Tarea2/src/Main.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 
-void main() {
-
+public class Main{
+    public static void main(String[] args){
+        Scanner read = new Scanner(System.in);
+        int[] notas = new int[5];
+        double avg;
+        int i=0;
+        System.out.println("Bienvenido al sistema de gestión de Calificaciones");
+        ingresoDeDatos(read, notas,  i);
+    }
+    static void ingresoDeDatos(Scanner read, int[] notas, int i){
+        System.out.println("Ingresa tu nombre");
+        String name = read.nextLine();
+        System.out.println("Ingresa tus notas (0-10): ");
+        for (i=0; i<5; i++ ){
+            if (read.hasNextInt() && notas[i]<10){
+                notas[i]=read.nextInt();
+            }
+            else{
+                System.out.println("El valor que has ingresado no es válido");
+                read.next();
+            }
+        }
+    }
 }
